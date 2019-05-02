25df2af73cf2d5f0a5c850ba99f989af60eec6cf
tpneumann <tneumann@uncc.edu>
2019-05-02 17:41:18 -0400

    Create file in WikiFiles folder

--- src/WikiSort.java
+++ src/WikiSort.java
@@ -1,8 +1,35 @@
+import java.io.File;
+import java.io.IOException;
 
 public class WikiSort {
 
 	public static void main(String[] args) {
-		
+		try {
+
+			String filename = "newFile.txt";
+			String workingDirectory = System.getProperty("user.dir");
+				
+			//****************//
+				
+			String absoluteFilePath = "";
+			
+			absoluteFilePath = workingDirectory + File.separator + "WikiFiles" + File.separator + filename;
+
+			System.out.println("Final filepath : " + absoluteFilePath);
+				
+			//****************//
+				
+			File file = new File(absoluteFilePath);
+
+			if (file.createNewFile()) {
+				System.out.println("File is created!");
+			} else {
+				System.out.println("File is already existed!");
+			}
+
+		  } catch (IOException e) {
+			e.printStackTrace();
+		  }
 	}
 
 }
